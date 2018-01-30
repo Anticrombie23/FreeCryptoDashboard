9e3cd52c986541539e60ee078fb2d1dd3c0c069d
Anticrombie23 <joshharm@yahoo.com>
2018-01-29 19:44:43 -0600

    initial setup

---
+++ FreeCryptoDashboard/src/main/java/com/freecryptodashboard/FreeCryptoDashboard/FreeCryptoDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.freecryptodashboard.FreeCryptoDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeCryptoDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeCryptoDashboardApplication.class, args);
+	}
+}
---
+++ FreeCryptoDashboard/src/test/java/com/freecryptodashboard/FreeCryptoDashboard/FreeCryptoDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.freecryptodashboard.FreeCryptoDashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FreeCryptoDashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
